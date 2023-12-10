3b8a4ec2a1bceaace1e8815256850b94e89301bd
SirRight12 <119626466+SirRight12@users.noreply.github.com>
2023-12-09 21:16:43 -0600

    Add comments and remove logs

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 async function pickAssetFolder() {
     try {
+        //get folder
         const folder = await window.showDirectoryPicker()
+        //iterate over the entries in a folder
         getAssetFiles(folder)
     } catch (err) {
 
     }
 }
-
+//stupid class, because I like classes
 class sFile {
     constructor (name,b64) {
         this.meta = [name.split(".")[0],b64]
     }
 }
 async function getAssetFiles(folder) {
+    //all assets that need to be added
     const assets = []
+    //all file entries
     const files = await folder.entries()
+    //iterate forever...
     while (true) {
+        //get the next file
         const file = await files.next()
+        //...well until there are no more files
         if (!file.value) break;
+        //seperate function for file.getFile(
         const actualFile = await handleAssetFile(file)
         const readFile = await read(actualFile)
         if (readFile) {
@@ -28,24 +36,28 @@ async function getAssetFiles(folder) {
 }
 
 async function read(file) {
+    //make it asynchronus
     return new Promise((resolve,reject) => {
         const reader = new FileReader()
         reader.onload = function (event) {
-            let res = event.target.result
-            // console.log(res)
             if (!res) {
                 reject("No base 64 for some reason")
             }
-            res = reader.result.toString()
+            //get the base64
+            let res = reader.result.toString()
+            //seperate the b64 from the garbage
             res = res.split(",")[1]
+            //add it to the filesJS object so it can be handled by files.js
             filesJS[file.name] = res
+            //handle it in files.js
             res = getFile(file.name)
-            console.log(res)
+            //create an sFile object, again, because I like classes
             const fileThing = new sFile(file.name,res)
+            //end the promise
             resolve(fileThing)
         }
         try {
-            console.log(file)
+            //if the file isn't a file then return false
             reader.readAsDataURL(file)
         } catch {
             resolve(false)
@@ -54,7 +66,9 @@ async function read(file) {
 }
 async function handleAssetFile(file) {
     try {
+        //iterate over file values in case of unexpected case where there are more than two values returned from a file
         const actualFile = await getActualFile(file)
+        //I could have just used return but I didn't
         if (!actualFile) throw new Error()
         return actualFile
     } catch {
@@ -66,32 +80,47 @@ async function getActualFile(file) {
         const fileX = file.value[x]
         //check if it's a file handle
         if (!fileX.name) continue
+        //return the file handle
         return fileX.getFile()
     }
+    //there is no file so return false
     return false
 }
+//make the js file so you can use it wherever 
 async function makeJSFile(assets) {
+    //create the files object
     let fileText = "const files = { \n \n}\n"
+    //iterate over all the assets
     for (let x = 0; x < assets.length; x ++) {
+        //use the data from an sFile
         const meta = assets[x].meta
         const name = meta[0]
         const b64 = meta[1]
+        //add it to the "files" object
         fileText += `files['${name}'] = "${b64}" \n`
     }
+    //make the assets.js file
     const file = new File([fileText],"assets.js")
+    //get the base64 for downloading
     const b64 = await getDownloadableJS(file)
+    //make an <a> element
     const link = document.createElement("a")
+    //set the name of the downloaded file
     link.download = "assets.js"
+    //give the data of the file
     link.href = b64
+    //download the file
     link.click()
 }
 async function getDownloadableJS(js) {
+    //make it asynchronus
     return new Promise((resolve,reject) => {
         const reader = new FileReader()
         reader.onload = function (event) {
             const res = event.target.result
             resolve(res)
         }
+        //read the file as base 64
         reader.readAsDataURL(js)
     }) 
 }
