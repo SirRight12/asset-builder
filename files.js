3fc0a1d1daaab2ebcc1cd2563e2df784e0b16356
SirRight12 <119626466+SirRight12@users.noreply.github.com>
2023-12-09 21:18:19 -0600

    remove handling for node apps

--- files.js
+++ files.js
@@ -13,16 +13,8 @@ const mime = {
     "xml": "image/svg+xml",
 }
 
-var warningGiven = false;
 
 function getFile(name) {
-    if (!window.location.href.replace(/^.*\:\/\//, "").includes(":") && !window.location.href.includes("file") || window.location.href.includes("localhost")) {
-        if (!warningGiven) {
-            console.log("Loading files from the server...");
-            warningGiven = true;
-        }
-        return "../static/threejs/media/" + name;
-    } else {
         if (filesJS[name] != undefined) {
             if (mime[name.split(".")[1]] != undefined) {
                 return "data:" + mime[name.split(".")[1]] + ";base64," + filesJS[name];
@@ -32,6 +24,4 @@ function getFile(name) {
         } else {
             console.error("'" + name + "' is not encoded in your files.js!");
         }
-    }
-}
-;
+    };
